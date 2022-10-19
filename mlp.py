f4f71102c0ebdabfa744a87ee244d40da404e1d3
qiben-jy <qb19@mails.tsinghua.edu.cn>
2022-10-19 15:01:56 +0800

    Initial commit

---
+++ mlp.py
@@ -0,0 +1,139 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import utils
+
+# input and output dimensions
+input_dim = 784
+output_dim = 10
+
+
+def calc_loss_and_grad(x, y, w1, b1, w2, b2, eval_only=False):
+    """Forward Propagation and Backward Propagation.
+
+    Given a mini-batch of images x, associated labels y, and a set of parameters, compute the
+    cross-entropy loss and gradients corresponding to these parameters.
+
+    :param x: images of one mini-batch.
+    :param y: labels of one mini-batch.
+    :param w1: weight parameters of layer 1.
+    :param b1: bias parameters of layer 1.
+    :param w2: weight parameters of layer 2.
+    :param b2: bias parameters of layer 2.
+    :param eval_only: if True, only return the loss and predictions of the MLP.
+    :return: a tuple of (loss, db2, dw2, db1, dw1)
+    """
+
+    # TODO
+    # forward pass
+    loss, y_hat = None, None
+
+    if eval_only:
+        return loss, y_hat
+
+    # TODO
+    # backward pass
+    db2, dw2, db1, dw1 = None, None, None, None
+
+    return loss, db2, dw2, db1, dw1
+
+
+def train(train_x, train_y, test_x, text_y, args: argparse.Namespace):
+    """Train the network.
+
+    :param train_x: images of the training set.
+    :param train_y: labels of the training set.
+    :param test_x: images of the test set.
+    :param text_y: labels of the test set.
+    :param args: a dict of hyper-parameters.
+    """
+
+    # TODO
+    #  randomly initialize the parameters (weights and biases)
+    w1, b1, w2, b2 = None, None, None, None
+
+    print('Start training:')
+    print_freq = 100
+    loss_curve = []
+
+    for epoch in range(args.epochs):
+        # train for one epoch
+        print("[Epoch #{}]".format(epoch))
+
+        # random shuffle dataset
+        dataset = np.hstack((train_x, train_y))
+        np.random.shuffle(dataset)
+        train_x = dataset[:, :input_dim]
+        train_y = dataset[:, input_dim:]
+
+        n_iterations = train_x.shape[0] // args.batch_size
+
+        for i in range(n_iterations):
+            # load a mini-batch
+            x_batch = train_x[i * args.batch_size: (i + 1) * args.batch_size, :]
+            y_batch = train_y[i * args.batch_size: (i + 1) * args.batch_size, :]
+
+            # TODO
+            # compute loss and gradients
+            loss = None
+
+            # TODO
+            # update parameters
+
+            loss_curve.append(loss)
+            if i % print_freq == 0:
+                print('[Iteration #{}/{}] [Loss #{:4f}]'.format(i, n_iterations, loss))
+
+    # show learning curve
+    plt.title('Training Curve')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.plot(range(len(loss_curve)), loss_curve)
+    plt.show()
+
+    # evaluate on the training set
+    loss, y_hat = calc_loss_and_grad(train_x, train_y, w1, b1, w2, b2, eval_only=True)
+    predictions = np.argmax(y_hat, axis=1)
+    labels = np.argmax(train_y, axis=1)
+    accuracy = np.sum(predictions == labels) / train_x.shape[0]
+    print('Top-1 accuracy on the training set', accuracy)
+
+    # evaluate on the test set
+    loss, y_hat = calc_loss_and_grad(test_x, text_y, w1, b1, w2, b2, eval_only=True)
+    predictions = np.argmax(y_hat, axis=1)
+    labels = np.argmax(text_y, axis=1)
+    accuracy = np.sum(predictions == labels) / test_x.shape[0]
+    print('Top-1 accuracy on the test set', accuracy)
+
+
+def main(args: argparse.Namespace):
+    # print hyper-parameters
+    print('Hyper-parameters:')
+    print(args)
+
+    # load training set and test set
+    train_x, train_y = utils.load_data("train")
+    test_x, text_y = utils.load_data("test")
+    print('Dataset information:')
+    print("training set size: {}".format(len(train_x)))
+    print("test set size: {}".format(len(test_x)))
+
+    # check your implementation of backward propagation before starting training
+    utils.check_grad(calc_loss_and_grad)
+
+    # train the network and report the accuracy on the training and the test set
+    train(train_x, train_y, test_x, text_y, args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Multilayer Perceptron')
+    parser.add_argument('--hidden-dim', default=50, type=int,
+                        help='hidden dimension of the Multilayer Perceptron')
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help='learning rate')
+    parser.add_argument('--batch-size', default=16, type=int,
+                        help='mini-batch size')
+    parser.add_argument('--epochs', default=10, type=int,
+                        help='number of total epochs to run')
+    args = parser.parse_args()
+    main(args)
---
+++ utils.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+def load_data(split):
+    """Load dataset.
+
+    :param split: a string specifying the partition of the dataset ('train' or 'test').
+    :return: a (images, labels) tuple of corresponding partition.
+    """
+
+    images = np.load("./data/mnist_{}_images.npy".format(split))
+    labels = np.load("./data/mnist_{}_labels.npy".format(split))
+    return images, labels
+
+
+def check_grad(calc_loss_and_grad):
+    """Check backward propagation implementation. This is naively implemented with finite difference method.
+    You do **not** need to modify this function.
+    """
+
+    def relative_error(z1, z2):
+        return np.mean((z1 - z2) ** 2 / (z1 ** 2 + z2 ** 2))
+
+    print('Gradient check of backward propagation:')
+
+    # generate random test data
+    x = np.random.rand(5, 15)
+    y = np.random.rand(5, 3)
+    # construct one hot labels
+    y = y * (y >= np.max(y, axis=1, keepdims=True)) / np.max(y, axis=1, keepdims=True)
+
+    # generate random parameters
+    w1 = np.random.rand(15, 3)
+    b1 = np.random.rand(3)
+    w2 = np.random.rand(3, 3)
+    b2 = np.random.rand(3)
+
+    # calculate grad by backward propagation
+    loss, db2, dw2, db1, dw1 = calc_loss_and_grad(x, y, w1, b1, w2, b2)
+
+    # calculate grad by finite difference
+    epsilon = 1e-5
+
+    numeric_dw2 = np.zeros_like(w2)
+    for i in range(w2.shape[0]):
+        for j in range(w2.shape[1]):
+            w2[i, j] += epsilon
+            loss_prime = calc_loss_and_grad(x, y, w1, b1, w2, b2)[0]
+            w2[i, j] -= epsilon
+            numeric_dw2[i, j] = (loss_prime - loss) / epsilon
+    print('Relative error of dw2', relative_error(numeric_dw2, dw2))
+
+    numeric_db2 = np.zeros_like(b2)
+    for i in range(db2.shape[0]):
+        b2[i] += epsilon
+        loss_prime = calc_loss_and_grad(x, y, w1, b1, w2, b2)[0]
+        b2[i] -= epsilon
+        numeric_db2[i] = (loss_prime - loss) / epsilon
+    print('Relative error of db2', relative_error(numeric_db2, db2))
+
+    numeric_dw1 = np.zeros_like(w1)
+    for i in range(w1.shape[0]):
+        for j in range(w1.shape[1]):
+            w1[i, j] += epsilon
+            loss_prime = calc_loss_and_grad(x, y, w1, b1, w2, b2)[0]
+            w1[i, j] -= epsilon
+            numeric_dw1[i, j] = (loss_prime - loss) / epsilon
+    print('Relative error of dw1', relative_error(numeric_dw1, dw1))
+
+    numeric_db1 = np.zeros_like(b1)
+    for i in range(db1.shape[0]):
+        b1[i] += epsilon
+        loss_prime = calc_loss_and_grad(x, y, w1, b1, w2, b2)[0]
+        b1[i] -= epsilon
+        numeric_db1[i] = (loss_prime - loss) / epsilon
+
+    print('Relative error of db1', relative_error(numeric_db1, db1))
+    print('If you implement back propagation correctly, all these relative errors should be less than 1e-5.')
+
